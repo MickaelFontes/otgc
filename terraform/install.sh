7100c85090566149408e24eb929b91b02bf12256
MF <maximicka@gmail.com>
2022-09-11 23:17:13 +0200

    Fix sed flag remove

--- terraform/install.sh
+++ terraform/install.sh
@@ -31,25 +31,25 @@ fi
 gsutil mb gs://tf_state_otgc_${random_chars}
 gcloud services enable cloudfunctions.googleapis.com
 sleep 30
-sed "s/\(MANUAL_EDIT_WRITE_YOUR_BUCKET\)/tf_state_otgc_${random_chars}/m" ./main.tf
+sed -i "s/\(MANUAL_EDIT_WRITE_YOUR_BUCKET\)/tf_state_otgc_${random_chars}/m" ./main.tf
 
 # Create terraform.tfvars.example
 cp terraform.tfvars.example terraform.tfvars
 
-sed "s/\(your_project\)/$1/m" ./terraform.tfvars
-sed "s/\(your_onboard_username\)/$6/m" ./terraform.tfvars
-sed "s/\(your_onboard_password\)/$7/m" ./terraform.tfvars
-sed "s/\(your_function\)/otgc_function_${random_chars}/m" ./terraform.tfvars
-sed "s/\(your_cron_expression\)/${random_int} \*\/1 \* \* \*\\ /m" ./terraform.tfvars
+sed -i "s/\(your_project\)/$1/m" ./terraform.tfvars
+sed -i "s/\(your_onboard_username\)/$6/m" ./terraform.tfvars
+sed -i "s/\(your_onboard_password\)/$7/m" ./terraform.tfvars
+sed -i "s/\(your_function\)/otgc_function_${random_chars}/m" ./terraform.tfvars
+sed -i "s/\(your_cron_expression\)/${random_int} \*\/1 \* \* \*\\ /m" ./terraform.tfvars
 
 # Update values in config.ini
 cd ../
 
 cp config_example.ini config.ini
-sed "s/\yourcalendar@group.calendar.google.com\)/$5/m" ./config.ini
-sed "s/\(your_client_id\)/$2/m" ./config.ini
-sed "s/\(your_client_secret\)/$3/m" ./config.ini
-sed "s/\(your_refresh_token\)/$4/m" ./config.ini
+sed -i "s/\yourcalendar@group.calendar.google.com\)/$5/m" ./config.ini
+sed -i "s/\(your_client_id\)/$2/m" ./config.ini
+sed -i "s/\(your_client_secret\)/$3/m" ./config.ini
+sed -i "s/\(your_refresh_token\)/$4/m" ./config.ini
 
 cd terraform
 
